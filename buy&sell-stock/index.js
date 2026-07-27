3140d2b66075dad780fb8c914c1bd196a99bb512
ayush-godhani <ayushgodhani456@gmail.com>
2026-07-27 21:48:35 +0530

    best time to buy and sell stock

---
+++ buy&sell-stock/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minprice = prices[0];
+    let maxprofit = 0;
+    for(let i=0; i<prices.length; i++){
+        if(prices[i] < minprice){
+            minprice = prices[i]
+        }
+        profit = prices[i] - minprice
+        if (profit > maxprofit){
+            maxprofit = profit;
+        }
+    }
+return maxprofit;
+};
