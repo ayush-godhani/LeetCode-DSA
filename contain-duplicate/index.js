63238e4f49140a34cee07d73c14d6e0f4b19aaa5
ayush-godhani <ayushgodhani456@gmail.com>
2026-07-27 21:25:05 +0530

    contain-duplicate problem easy

---
+++ contain-duplicate/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i] === nums[j])
+                return true;
+           
+        }
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1,2,4,5,1]));
