8af19b3bbf05135e4d3da7fb869db930033fa25e
ayush-godhani <ayushgodhani456@gmail.com>
2026-07-28 13:21:17 +0530

    find single number from array

---
+++ findsingle-number/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let singleNum = 0
+
+    for(let num of nums){
+        singleNum =singleNum ^ nums
+    }
+    return singleNum
+    
+};
