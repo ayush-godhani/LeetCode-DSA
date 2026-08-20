5c791138fa51cd5d813632f58ee1fd45fedc536d
ayush-godhani <ayushgodhani456@gmail.com>
2026-08-21 00:24:07 +0530

    effient version of duplicate

--- contain-duplicate/index.js
+++ contain-duplicate/index.js
@@ -1,16 +1,30 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
-var containsDuplicate = function(nums) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i] === nums[j])
-                return true;
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// var containsDuplicate = function(nums) {
+//     for(let i=0; i<nums.length; i++){
+//         for(let j=i+1; j<nums.length; j++){
+//             if(nums[i] === nums[j])
+//                 return true;
            
+//         }
+//     }
+//     return false;
+// };
+
+ //console.log(containsDuplicate([1,2,4,5,1]));
+
+var containsDuplicate = function(nums){
+    let set = new Set();
+
+    for(let num of nums){
+        if(set.has(num)){
+            return true;
         }
+        set.add(num);
     }
     return false;
-};
+}
 
 console.log(containsDuplicate([1,2,4,5,1]));
